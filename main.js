a095782860ddf0a5014f9508e786ebc169d97317
Aivaras Šimulis <aivaras.simulis@amidus.lt>
2018-06-29 10:14:22 +0300

    Disable re-rendering with v-once

---
+++ main.js
@@ -0,0 +1,16 @@
+new Vue({
+    el: '#app',
+    data: {
+        title: 'Hello World!',
+        link: 'http://google.com'
+    },
+    methods: {
+        changeTitle: function (event) {
+            this.title = event.target.value;
+        },
+        sayHello() {
+            this.title = 'Hello!';
+            return this.title;
+        }
+    }
+});
