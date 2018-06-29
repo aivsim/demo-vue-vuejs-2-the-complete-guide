30d0b3a57a74e0771c37f15816a18485bf3a70c0
Aivaras Šimulis <aivaras.simulis@amidus.lt>
2018-06-29 10:19:22 +0300

    How to output raw HTML with v-html

--- main.js
+++ main.js
@@ -2,12 +2,10 @@ new Vue({
     el: '#app',
     data: {
         title: 'Hello World!',
-        link: 'http://google.com'
+        link: 'http://google.com',
+        finishedLink: '<a href="http://google.com">Google</a>'
     },
     methods: {
-        changeTitle: function (event) {
-            this.title = event.target.value;
-        },
         sayHello() {
             this.title = 'Hello!';
             return this.title;
